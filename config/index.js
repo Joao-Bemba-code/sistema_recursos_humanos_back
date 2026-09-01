ec5831516c5654c47e9223b730c59703ce85fb79
grupokamatambu <kamatambu.oficial@gmail.com>
2026-09-01 12:41:10 +0100

    chore: config BD usa Port_database da env (TiDB porta 4000)

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
 var dotenv = require('dotenv');
 dotenv.config({path: __dirname + '/../.env'});
 
-var {Name_database, User_database, Pass_database, Host_database, Lang_database} = process.env;
+var {Name_database, User_database, Pass_database, Host_database, Port_database, Lang_database} = process.env;
 
 var Sequelize = require('sequelize');
 
 var sequelize = new Sequelize(Name_database, User_database, Pass_database, {
     host: Host_database,
     dialect: Lang_database,
-    port: 4076,
+    port: parseInt(Port_database) || 4076,
     dialectOptions: {
         ssl: {
             require: true,
